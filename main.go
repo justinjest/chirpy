2e4e061d97eac24a580694ef28f028dce5245686
justinjest <jest.cayaban@gmail.com>
2024-11-06 11:50:29 +0900

    Fixed error with .env file naming

--- main.go
+++ main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
 
+	"github.com/joho/godotenv"
+	"github.com/justinjest/chirpy/internal/database"
 	jsonParser "github.com/justinjest/chirpy/internal/json"
 	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	database       *database.Queries
 }
 
 func main() {
+
 	const filepathRoot = "."
 	const port = "8080"
-
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Unable to load .env file")
+	}
+	dbUrl := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatalf("Unable to load postgress file")
+	}
+	dbQueries := database.New(db)
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		database:       dbQueries,
 	}
 	mux := http.NewServeMux()
 	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))
